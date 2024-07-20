04579e4d7a459a28c323f795731033dbc342bf1b
Stuart6557 <vivianliu532@gmail.com>
2024-07-19 18:33:02 -0700

    finished lab

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express"),
-       app = express(),
-       port = process.env.PORT || 8080,
-       cors = require("cors");
+        app = express(),
+        port = process.env.PORT || 8080,
+        cors = require("cors");
 const bodyParser = require('body-parser');
 const fsPromises = require("fs").promises;
 //const fs = require("fs");
@@ -9,16 +9,26 @@ const todoDBName = "tododb";
 const useCloudant = true;
 
 
-
 //Init code for Cloudant
 const {CloudantV1} = require('@ibm-cloud/cloudant');
-if (useCloudant)
-{
+if (useCloudant) {
     initDB();
 }
 
 
-app.use(cors());
+const basicAuth = require("express-basic-auth");
+var { authenticator, upsertUser, cookieAuth } = require("./authentication");
+const auth = basicAuth({
+    authorizer: authenticator
+});
+const cookieParser = require("cookie-parser");
+app.use(cookieParser("82e4e438a0705fabf61f9854e3b575af"));
+
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
 app.use(bodyParser.json({ extended: true }));
 
 app.listen(port, () => console.log("Backend server live on " + port));
@@ -40,10 +50,10 @@ async function addItem (request, response) {
         const curDate = request.body.jsonObject.currentDate
         const dueDate = request.body.jsonObject.dueDate
         const newTask = {
-          ID: id,
-          Task: task,
-          Current_date: curDate,
-          Due_date: dueDate
+            ID: id,
+            Task: task,
+            Current_date: curDate,
+            Due_date: dueDate
         }
         
         if (useCloudant) {
@@ -63,8 +73,8 @@ async function addItem (request, response) {
             const client = CloudantV1.newInstance({});
             console.log('Writing to: ', todoDBName)
             const createDocumentResponse = await client.postDocument({
-              db: todoDBName,
-              document: todoDocument,
+                db: todoDBName,
+                document: todoDocument,
             });
             console.log('Successfully wrote to cloudant DB');
         } else {
@@ -89,23 +99,21 @@ async function getItems (request, response) {
 
     //begin cloudant here
     if (useCloudant) {
-    //add for cloudant client
-    const client = CloudantV1.newInstance({});
-    var listofdocs;
-    await client.postAllDocs({
-        db: todoDBName,
-        includeDocs: true
-    }).then(response => {
-        listofdocs=response.result;
+        //add for cloudant client
+        const client = CloudantV1.newInstance({});
+        var listofdocs;
+        await client.postAllDocs({
+            db: todoDBName,
+            includeDocs: true
+        }).then(response => {
+            listofdocs=response.result;
         });
-    response.json(JSON.stringify(listofdocs));
+        response.json(JSON.stringify(listofdocs));
+    } else {
+        //for non-cloudant use-case
+        var data = await fsPromises.readFile("database.json");
+        response.json(JSON.parse(data));
     }
-    else {
-    //for non-cloudant use-case
-    var data = await fsPromises.readFile("database.json");
-    response.json(JSON.parse(data));
-    }
-
 };
 
 //** week 6, search items service */
@@ -118,22 +126,20 @@ async function searchItems (request, response) {
         const client = CloudantV1.newInstance({});
         var search_results
         await client.postSearch({
-            db: todoDBName,
-            ddoc: 'newdesign',
-            query: 'task:'+searchField,
-            index: 'newSearch'
-          }).then(response => {
-            search_results=response.result;
-            console.log(response.result);
-          });
+                db: todoDBName,
+                ddoc: 'newdesign',
+                query: 'task:'+searchField,
+                index: 'newSearch'
+            }).then(response => {
+                search_results=response.result;
+                console.log(response.result);
+            });
         console.log(search_results);
         response.json(JSON.stringify(search_results));
-        
-    }
-    else {
-    var json = JSON.parse (await fsPromises.readFile("database.json"));
-    var returnData = json.filter(jsondata => jsondata.Task === searchField);
-    response.json(returnData);
+    } else {
+        var json = JSON.parse (await fsPromises.readFile("database.json"));
+        var returnData = json.filter(jsondata => jsondata.Task === searchField);
+        response.json(returnData);
     }
 };
 
@@ -148,17 +154,34 @@ async function initDB ()
         const client = CloudantV1.newInstance({});
         const putDatabaseResult = (
         await client.putDatabase({
-        db: todoDBName,
-      })
-    ).result;
-    if (putDatabaseResult.ok) {
-      console.log(`"${todoDBName}" database created.`);
+            db: todoDBName,
+        })
+        ).result;
+        if (putDatabaseResult.ok) {
+            console.log(`"${todoDBName}" database created.`);
+        }
+    } catch (err) {
+        console.log(
+            `Cannot create "${todoDBName}" database, err: "${err.message}".`
+        );
     }
-  } catch (err) {
-   
-      console.log(
-        `Cannot create "${todoDBName}" database, err: "${err.message}".`
-      );
-
-  }
 };
+
+
+app.get("/authenticate", auth, (req, res) => {
+    console.log(`user logging in: ${req.auth.user}`);
+    res.cookie('user', req.auth.user, { signed: true });
+    res.sendStatus(200);
+});
+
+app.post("/users", (req, res) => {
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+    const upsertSucceeded = upsertUser(username, password)
+    res.sendStatus(upsertSucceeded ? 200 : 401);
+});
+
+app.get("/logout", (req, res) => {
+    res.clearCookie('user');
+    res.end();
+});
